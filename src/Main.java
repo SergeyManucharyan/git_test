fdec75733d4e77d6ad060f01edc2be73c9242288
Sergey <sergey.garik.areg@gmail.com>
2023-01-28 21:09:52 +0400

    added if statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Input first number");
+        int numOne= sc.nextInt();
+        System.out.println("Input second number");
+        int numTwo=sc.nextInt();
+        if(numTwo>numOne){
+            System.out.println("Second number is great");
+        }else if(numTwo<numOne){
+            System.out.println("First number is great");
+        }else{
+            System.out.println("Two numbers is equal");
+        }
     }
 }
